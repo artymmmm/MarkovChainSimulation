16ee5cda2823fecb9ea899dcc323a129fb986294
artymmmm <arty1303@gmail.com>
2022-09-20 22:10:49 +0300

    Added calculations by steps

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import numpy as np
 
 # функция одной симуляции
 # параметры: матрица переходных вероятностей,
-# количество шагов, изначальное состояниеразмер матрицы
+# количество шагов, изначальное состояние, размер матрицы
 def launch(probability_matrix, steps, first_state, matrix_size):
     state = first_state
+    # цикл по шагам, где выбирается следующее состояние в соответствии со строкой
+    # матрицы вероятностей
     for i in range(steps):
-        prob = np.random.choice(matrix_size, p=probability_matrix[state])
-        state = prob
+        state = np.random.choice(matrix_size, p=probability_matrix[state])
     return state
 
 
@@ -43,7 +44,7 @@ for i in range(simulations):
 modelPr = goodEndCount / launchCount
 
 # вычисление по формуле Pr = Pr0 * (ProbMatrix**steps)
-initialPr = np.zeros(matrixSize)
+initialPr = np.zeros(matrixSize) # вектор нулей
 initialPr[firstState] = 1  # начальный вектор
 transitionPrToPower = np.linalg.matrix_power(probabilityMatrix,
                                              steps)  # степень
@@ -54,3 +55,12 @@ finalStatePr = finalPr[lastState]
 print('''\nВероятность попадания в состояние {0} на шаге {1}:\n1) {2}, 
 полученная с помощью вычислений\n2) {3}, полученная с помощью 
 моделирования'''.format(lastState, steps, finalStatePr, modelPr))
+
+for steps_i in range(steps):
+   transitionPrToPower = np.linalg.matrix_power(probabilityMatrix,
+                                              steps_i)  # степень
+   finalPr = np.matmul(initialPr,
+                       transitionPrToPower)  # умножение вектора на матрицу
+   # finalStatePr = finalPr[lastState]
+   print('''\nВероятность на шаге {0}:\n{1},
+полученная с помощью вычислений'''.format(steps_i + 1, finalPr))
